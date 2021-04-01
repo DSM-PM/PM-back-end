2e326df495fcdd915d12b1c99cc661a407683008
Srim <deamoon0801@naver.com>
2021-04-01 10:03:31 +0900

    [ADD] cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
 const { sequelize } = require("./models");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 require("dotenv").config();
 app.use("/", routes);
 sequelize.sync();
